e56615fff3be12d2b73ddbd57c8e9d406f25ae26
Sayed Hassan <sayedhassan.ny@gmail.com>
2024-02-26 21:38:18 +0000

    completed all tasks.

--- events.js
+++ events.js
@@ -3,6 +3,7 @@
 
 let hoverCard = document.querySelector('[data-secret="hover"]');
 // 1. Add an event listener to reveal the image on hover.
+hoverCard.addEventListener('mouseover', function()
 //  - The selector is hoverCard
 //  - The event type is mouseover
 {
@@ -10,9 +11,10 @@ let hoverCard = document.querySelector('[data-secret="hover"]');
     image.style.width = '100%';
     image.style.height = '100%';
     image.style.opacity = '1';
-}
+});
 
 // 2. Add an event listener to make the image hidden again.
+  hoverCard.addEventListener('mouseout', function()
 //  - The selector is hoverCard
 //  - The event type is mouseout
 {
@@ -20,42 +22,47 @@ let hoverCard = document.querySelector('[data-secret="hover"]');
     image.style.width = '0';
     image.style.height = '0';
     image.style.opacity = '0';
-}
+});
+
 
 
 
 const clickCard = document.querySelector('[data-secret="click"]');
 // 3. Add an event listener to reveal and animate the shape on click.
+clickCard.addEventListener('click', function()
 //  - The selector is clickCard
 //  - The event type is click
 {
     clickCard.classList.toggle('revealed');
-}
+});
 
 
 const doubleClickCard = document.querySelector('[data-secret="double-click"]');
 // 4. Add an event listener to enlarge the text on double click.
+doubleClickCard.addEventListener('dblclick',function()
 //  - The selector is doubleClickCard
 //  - The event type is dblclick
 {
     doubleClickCard.classList.toggle('revealed');
-}
+});
 
 
 const keypressCard = document.querySelector('[data-secret="keypress"]');
 // 5. Add an event listener to shake the card on any keypress.
+document.addEventListener('keydown', function()
 //  - The selector is document
 //  - The event type is keydown
 {
     keypressCard.classList.add('revealed');
     keypressCard.style.animation = "shake 0.5s";
-}
+});
 
 
 // 6. Add an event listener to stop the shaking.
+document.addEventListener('keyup', function()
 //  - The selector is document
 //  - The event type is keyup
 {
     keypressCard.classList.remove('revealed');
     keypressCard.style.animation = "";
-}
+});
